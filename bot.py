2004fbdb7123979b3fa534c08e400a1a6da6193b
deisign <deisign@gmail.com>
2025-01-17 12:48:30 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 import os
 
@@ -7,20 +7,18 @@ import os
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 # Команда /start
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text("Привет! Я готов работать!")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Привет! Я готов работать!")
 
-# Настройка бота
+# Настройка приложения
 def main():
-    updater = Updater(BOT_TOKEN)
-    dispatcher = updater.dispatcher
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
 
     # Добавляем обработчик команды /start
-    dispatcher.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("start", start))
 
     # Запускаем бота
-    updater.start_polling()
-    updater.idle()
+    application.run_polling()
 
 if __name__ == "__main__":
     main()
